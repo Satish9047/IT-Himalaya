48719da2efbdf0c541c5cea752d990557003d610
Satish9047 <satishprajapati930@gmail.com>
2024-12-02 21:48:23 +0545

    05: Add LocalStorage for persisting the task + minor change in CSS and Html

--- task1--html,css,jquery/script.js
+++ task1--html,css,jquery/script.js
@@ -1,5 +1,25 @@
 $("document").ready(function () {
   const tasks = [];
+  const storedTasks = JSON.parse(localStorage.getItem("tasks"));
+  console.log("stored task", storedTasks, typeof storedTasks);
+  if (storedTasks?.length > 0) {
+    storedTasks.forEach((task) => {
+      const newTask = new Task(
+        task.id,
+        task.description,
+        task.createdAt,
+        task.dueDate
+      );
+      tasks.push(newTask);
+    });
+    renderTasks(tasks);
+    renderCompletedTasks(tasks);
+  }
+  $("#taskDescription").on("keypress", function (event) {
+    if (event.key === "Enter") {
+      $("#addTaskBtn").click();
+    }
+  });
 
   $("#addTaskBtn").click(function () {
     const taskDescription = $("#taskDescription").val();
@@ -12,6 +32,8 @@ $("document").ready(function () {
       const newTask = new Task(id, taskDescription, createdAt, dueDate);
       tasks.push(newTask);
       $("#taskDescription").val("");
+      console.log("create task", tasks);
+      localStorage.setItem("tasks", JSON.stringify(tasks));
     } else {
       alert("Please enter a task description");
     }
@@ -27,6 +49,7 @@ $("document").ready(function () {
     console.log("task info", task);
     task.toggleCompleted();
     task.completedAt = getFormattedDate();
+    localStorage.setItem("tasks", JSON.stringify(tasks));
     renderCompletedTasks(tasks);
     renderTasks(tasks);
   });
@@ -36,6 +59,7 @@ $("document").ready(function () {
     const taskId = $(this).closest("li").data("id");
     console.log("delete task id: ", taskId);
     deleteTask(tasks, taskId);
+    localStorage.setItem("tasks", JSON.stringify(tasks));
     renderTasks(tasks);
   });
 
@@ -43,6 +67,7 @@ $("document").ready(function () {
   $("#completedList").on("click", ".delete", function () {
     const taskId = $(this).closest("li").data("id");
     console.log("delete task id: ", taskId);
+    localStorage.setItem("tasks", JSON.stringify(tasks));
     deleteCompletedTask(tasks, taskId);
   });
 });
