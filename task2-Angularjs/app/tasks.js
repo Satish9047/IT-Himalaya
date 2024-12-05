597b7e3f801a367ed2d79b543ce4109cb1d0fc61
Satish9047 <satishprajapati930@gmail.com>
2024-12-05 12:29:12 +0545

    30: Add Task Class

---
+++ task2-Angularjs/app/tasks.js
@@ -0,0 +1,21 @@
+class Task {
+  constructor(
+    id,
+    description,
+    createdAt,
+    dueDate,
+    completed = false,
+    completedAt = null
+  ) {
+    (this.id = id), (this.description = description);
+    this.createdAt = createdAt;
+    this.dueDate = dueDate;
+    this.completed = completed;
+    this.completedAt = completedAt;
+  }
+
+  toggleComplete() {
+    this.completed = true;
+    this.completedAt = getFormattedDate();
+  }
+}
