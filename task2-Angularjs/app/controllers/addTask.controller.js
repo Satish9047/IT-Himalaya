0e1306ee177143b6caa90029052986a72919b139
Satish9047 <satishprajapati930@gmail.com>
2024-12-06 20:10:06 +0545

    56: add Validate + empty input after submit

--- task2-Angularjs/app/controllers/addTask.controller.js
+++ task2-Angularjs/app/controllers/addTask.controller.js
@@ -8,16 +8,17 @@ taskManager.controller("addTaskController", [
 
     this.addTask = function () {
       console.log(this.taskDescription);
+      if (this.taskDescription.trim() !== "") {
+        const id = Date.now().toString();
+        const description = this.taskDescription;
+        const createdAt = getFormattedDate();
+        const dueDate = getDueDate();
 
-      const id = Date.now().toString();
-      const description = this.taskDescription;
-      const createdAt = getFormattedDate();
-      const dueDate = getDueDate();
+        const newTask = new Task(id, description, createdAt, dueDate);
 
-      const newTask = new Task(id, description, createdAt, dueDate);
-
-      taskService.addTask(newTask);
-      this.taskDescription = "";
+        taskService.addTask(newTask);
+        this.taskDescription = "";
+      }
     };
   },
 ]);
