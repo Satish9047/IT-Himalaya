2c67496c63cb48b88cd4b9ae6bf386cad1e86105
Satish9047 <satishprajapati930@gmail.com>
2024-12-06 14:34:48 +0545

    41: Add addTask Template + controller

---
+++ task2-Angularjs/app/controllers/addTask.controller.js
@@ -0,0 +1,21 @@
+taskManager.controller("addTaskController", [
+  "$scope",
+  "taskService",
+  function ($scope, taskService) {
+    const $AddCtrl = this;
+    this.taskDescription = "";
+
+    this.addTask = function () {
+      console.log(this.taskDescription);
+
+      const id = Date.now().toString();
+      const description = this.taskDescription;
+      const createdAt = getFormattedDate();
+      const dueDate = getDueDate();
+
+      const newTask = new Task(id, description, createdAt, dueDate);
+
+      taskService.addTask(newTask);
+    };
+  },
+]);
