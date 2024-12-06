f7aecb0e87dca53307788a9476ff92d3b5635312
Satish9047 <satishprajapati930@gmail.com>
2024-12-06 16:31:41 +0545

    50: Add loadtasks function to load the task from localStorage

--- task2-Angularjs/app/services/tasks.service.js
+++ task2-Angularjs/app/services/tasks.service.js
@@ -1,21 +1,59 @@
-taskManager.service("taskService", function () {
-  const tasks = [];
+taskManager.service("taskService", [
+  "localForage",
+  function (localForage) {
+    const tasks = [
+      {
+        id: 1,
+        description: "Task 1",
+        createdAt: "2023-08-01",
+        dueDate: "2023-08-05",
+        completed: false,
+        completedAt: null,
+      },
+    ];
 
-  this.getTasks = function () {
-    return tasks;
-  };
+    function loadTasks() {
+      getTasksFromLocalForage().then((tasks) => {
+        if (tasks.length > 0) {
+          tasks.forEach((task) => {
+            const loadTask = new Task(
+              task.id,
+              task.description,
+              task.createdAt,
+              task.dueDate,
+              task.completed || false,
+              task.completedAt || null
+            );
+            this.tasks.push(loadTask);
+          });
+        }
+      });
+    }
 
-  this.addTask = function (newTask) {
-    tasks.push(newTask);
-  };
+    loadTasks();
 
-  this.deleteTask = function (taskId) {
-    const taskIndex = tasks.findIndex((task) => task.id == taskId);
-    tasks.splice(taskIndex, 1);
-  };
+    this.getTasks = function () {
+      console.log("Tasks get:", tasks);
 
-  this.sendToCompleted = function (taskId) {
-    const task = tasks.find((task) => task.id === taskId);
-    task.toggleToCompletedTask();
-  };
-});
+      return tasks;
+    };
+
+    this.addTask = function (newTask) {
+      tasks.push(newTask);
+      setTaskToLocalForage(tasks, localForage);
+      console.log("Tasks add:", tasks);
+    };
+
+    this.deleteTask = function (taskId) {
+      const taskIndex = tasks.findIndex((task) => task.id == taskId);
+      tasks.splice(taskIndex, 1);
+      setTaskToLocalForage(tasks, localForage);
+    };
+
+    this.sendToCompleted = function (taskId) {
+      const task = tasks.find((task) => task.id === taskId);
+      task.toggleToCompletedTask(task);
+      setTaskToLocalForage(tasks, localForage);
+    };
+  },
+]);
