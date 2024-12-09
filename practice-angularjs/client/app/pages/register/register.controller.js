bc55ec1db841cf1e79a189cbdaac7645c3a92d56
Satish9047 <satishprajapati930@gmail.com>
2024-12-09 16:03:16 +0545

    73: Update Register Ui and Controller

--- practice-angularjs/client/app/pages/register/register.controller.js
+++ practice-angularjs/client/app/pages/register/register.controller.js
@@ -2,11 +2,13 @@ app.component("appRegister", {
   templateUrl: "./app/pages/register/register.template.html",
   controllerAs: "$RegisterCtrl",
   controller: [
-    "$scope",
-    function ($scope) {
-      const $RegisterCtrl = this;
-      $RegisterCtrl.message = "Welcome to Register";
-      console.log("This is register page");
+    function () {
+      this.user = {};
+
+      this.onRegister = (event) => {
+        event.preventDefault();
+        console.log(this.user);
+      };
     },
   ],
 });
