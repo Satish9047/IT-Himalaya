c3544c16ab849ff57b40f9bfd913f8ba6182e7a6
Satish9047 <satishprajapati930@gmail.com>
2024-12-10 12:48:00 +0545

    88: Update userService  with proper naming + remove initial data

--- practice-angularjs/client/app/services/user.service.js
+++ practice-angularjs/client/app/services/user.service.js
@@ -1,35 +1,30 @@
 app.service("userService", [
   "localForage",
   function (localforage) {
-    this.user = [
-      {
-        fname: "admin",
-        lname: "admin",
-        email: "admin@gmail.com",
-        password: "admin123",
-      },
-    ];
+    this.user = {};
 
     this.getUser = () => {
-      return this.user.map((user) => {
-        const storedName = user.email.replace("@", "").replace(".", "");
-        const storeInstance = localforage.createInstance({
-          name: `User_${storedName}`,
-          storeName: storedName,
-          description: `Data for ${user.email}`,
-        });
+      if (this.user.email) {
+        return this.user;
+      }
 
-        return storeInstance.getItem(user.email).then((storedUserData) => {
-          if (storedUserData) {
-            return { ...storedUserData, password: undefined };
-          }
-          return null;
-        });
+      const storedName = user.email.replace("@", "").replace(".", "");
+      const storeInstance = localforage.createInstance({
+        name: `loggedUser`,
+        storeName: storedName,
+        description: `Data for ${user.email}`,
       });
 
-      // return this.user.map((user) => {
-      //   delete user.password;
-      // });
+      return storeInstance.getItem(user.email).then((storedUserData) => {
+        if (storedUserData) {
+          return { ...storedUserData, password: undefined };
+        }
+        return null;
+      });
+    };
+
+    this.setUser = (user) => {
+      this.user = user;
     };
   },
 ]);
