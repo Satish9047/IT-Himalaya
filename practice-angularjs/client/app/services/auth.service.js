efaf0deb6fadf75ec8a03c260a515235c4fd24d1
Satish9047 <satishprajapati930@gmail.com>
2024-12-10 16:00:24 +0545

    108: Add experiment function for loginUser

--- practice-angularjs/client/app/services/auth.service.js
+++ practice-angularjs/client/app/services/auth.service.js
@@ -1,7 +1,5 @@
 app.service("authService", [
   function () {
-    this.user = [];
-
     //Register User
     this.registerUser = (user) => {
       //setup localforage instance
@@ -10,7 +8,8 @@ app.service("authService", [
       // Store user data in Users store
       return storeInstance
         .setItem("userDetails", user)
-        .then(() => {
+        .then((resData) => {
+          console.log(resData);
           console.log(`User ${user.email} data stored successfully.`);
           return true;
         })
@@ -56,5 +55,42 @@ app.service("authService", [
         }
       });
     };
+
+    //experiment
+
+    // this.loginUser = (user) => {
+    //   //setup localforage instance
+    //   const storeInstance = getStoreInstance(user);
+
+    //   // Retrieve user data from Users store
+    //   return storeInstance.getItem("userDetails").then((storedUserData) => {
+    //     if (storedUserData && storedUserData.password === user.password) {
+    //       //Setting the user into the loggedUser store if the user in valid
+    //       storedUserData.isLoggedIn = true;
+
+    //       //Setting the  user in loggedUser store
+    //       storeInstance
+    //         .setItem("userDetails", storedUserData)
+    //         .then(() => {
+    //           console.log(
+    //             `User ${storedUserData.email} data stored successfully in loggedUser.`
+    //           );
+    //           return true;
+    //         })
+    //         .catch((err) => {
+    //           console.error(
+    //             `Error storing user data for ${storedUserData.email}:`,
+    //             err
+    //           );
+    //           return false;
+    //         });
+
+    //       return { storeInstance: storeInstance, user: storedUserData };
+    //     } else {
+    //       console.log("log fail");
+    //       return false;
+    //     }
+    //   });
+    // };
   },
 ]);
