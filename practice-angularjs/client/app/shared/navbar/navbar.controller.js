06fe78d948ccf7fb87220909ab4bcd1001752617
Satish9047 <satishprajapati930@gmail.com>
2024-12-10 12:55:48 +0545

    95: Add the state, userService, scope so that  component can get he user object and rerender according to it

--- practice-angularjs/client/app/shared/navbar/navbar.controller.js
+++ practice-angularjs/client/app/shared/navbar/navbar.controller.js
@@ -2,11 +2,32 @@ app.component("appNavbar", {
   templateUrl: "./app/shared/navbar/navbar.template.html",
   controllerAs: "$Ctrl",
   controller: [
-    function (userService) {
-      this.user = {};
+    "$scope",
+    "$state",
+    "userService",
+    function ($scope, $state, userService) {
       this.isLoggedIn = false;
+      this.user = {};
 
-      // this.user = userService.getUser();
+      const initializeData = (user) => {
+        if (user) {
+          this.user = user;
+          this.isLoggedIn = true;
+        } else {
+          this.isLoggedIn = false;
+          this.user = {};
+          $state.go("login");
+        }
+      };
+
+      userService.getUser().then((user) => {
+        initializeData(user);
+      });
+
+      $scope.$on("user:updated", (event, user) => {
+        initializeData(user);
+        $scope.$apply();
+      });
     },
   ],
 });
