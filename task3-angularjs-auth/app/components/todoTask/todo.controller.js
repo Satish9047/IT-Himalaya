5014be80a96b2ba498527fed70a5e6dfe754b02d
Satish9047 <satishprajapati930@gmail.com>
2024-12-11 14:29:08 +0545

    118: Add controller and template for todo component

---
+++ task3-angularjs-auth/app/components/todoTask/todo.controller.js
@@ -0,0 +1,32 @@
+app.component("todoTask", {
+  templateUrl: "./app/components/todoTask/todo.template.html",
+  controllerAs: "$TodoCtrl",
+  controller: [
+    "$scope",
+    "taskService",
+    function ($scope, taskService) {
+      this.taskList = [];
+
+      taskService.loadTask().then((tasks) => {
+        this.taskList = tasks;
+        // console.log("todo", $TodoCtrl.taskList);
+        $scope.$apply();
+      });
+
+      // Get tasks from the service
+      this.taskList = taskService.getTasks();
+      console.log("todo--", this.taskList);
+      //   console.log("todo", $TodoCtrl.taskList);
+
+      this.deleteTask = (taskId) => {
+        taskService.deleteTask(taskId);
+        this.taskList = taskService.getTasks();
+      };
+
+      this.sendTaskToCompleted = (taskId) => {
+        taskService.sendToCompleted(taskId);
+        this.taskList = taskService.getTasks();
+      };
+    },
+  ],
+});
