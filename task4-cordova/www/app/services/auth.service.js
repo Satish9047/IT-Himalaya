ab3c12d1c5fce5a0847b69213173f5c269c04f4e
Satish9047 <satishprajapati930@gmail.com>
2024-12-17 13:57:36 +0545

    149: Update code by using storageService

--- task4-cordova/www/app/services/auth.service.js
+++ task4-cordova/www/app/services/auth.service.js
@@ -1,48 +1,58 @@
 app.service("authService", [
-  function () {
-    //Register User
+  "storageService",
+  function (storageService) {
     this.registerUser = (user) => {
-      const storeInstance = getStoreInstance(user);
-
-      // Store user data in Users store
-      return storeInstance
-        .setItem("userDetails", user)
+      return storageService
+        .registerNewUser(user)
         .then((resData) => {
-          console.log(`User ${user.email} data stored successfully.`);
+          console.log("User registered successfully:", resData);
           return true;
         })
-        .catch((err) => {
-          console.error(`Error storing user data for ${user.email}:`, err);
+        .catch((error) => {
+          console.error("Error registering user:", error);
           return false;
         });
     };
 
     //Login User
     this.loginUser = (user) => {
-      const storeInstance = getStoreInstance(user);
-      return storeInstance.getItem("userDetails").then((storedUserData) => {
-        if (storedUserData && storedUserData.password === user.password) {
-          const storeInstance = getLoggedUserStoreInstance();
-
-          storeInstance
-            .setItem("loggedUser", storedUserData)
-            .then(() => {
-              console.log(
-                `User ${user.email} data stored successfully in loggedUser.`
-              );
-              return true;
-            })
-            .catch((err) => {
-              console.error(`Error storing user data for ${user.email}:`, err);
-              return false;
-            });
-
-          return { storeInstance: storeInstance, user: storedUserData };
-        } else {
-          console.log("log fail");
+      return storageService
+        .loginUser(user)
+        .then((resData) => {
+          console.log("User logged in successfully:", resData);
+          return true;
+        })
+        .catch((error) => {
+          console.error("Error logging in user:", error);
           return false;
-        }
-      });
+        });
     };
+
+    // this.loginUser = (user) => {
+    //   const storeInstance = getStoreInstance(user);
+    //   return storeInstance.getItem("userDetails").then((storedUserData) => {
+    //     if (storedUserData && storedUserData.password === user.password) {
+    //       const storeInstance = getLoggedUserStoreInstance();
+
+    //       storeInstance
+    //         .setItem("loggedUser", storedUserData)
+    //         .then(() => {
+    //           console.log(
+    //             `User ${user.email} data stored successfully in loggedUser.`
+    //           );
+    //           return true;
+    //         })
+    //         .catch((err) => {
+    //           console.error(`Error storing user data for ${user.email}:`, err);
+    //           return false;
+    //         });
+
+    //       return { storeInstance: storeInstance, user: storedUserData };
+    //     } else {
+    //       console.log("log fail");
+    //       return false;
+    //     }
+    //   });
+    // };
   },
 ]);
