88e1fd275c1b1d19d53cc2ba54dd8481d5421563
Satish9047 <satishprajapati930@gmail.com>
2024-12-18 18:44:40 +0545

    166: Update build file

---
+++ task4-cordova/platforms/android/app/src/main/assets/www/cordova_plugins.js
@@ -0,0 +1,24 @@
+cordova.define('cordova/plugin_list', function(require, exports, module) {
+  module.exports = [
+    {
+      "id": "cordova-sqlite-storage.SQLitePlugin",
+      "file": "plugins/cordova-sqlite-storage/www/SQLitePlugin.js",
+      "pluginId": "cordova-sqlite-storage",
+      "clobbers": [
+        "SQLitePlugin"
+      ]
+    },
+    {
+      "id": "cordova-plugin-device.device",
+      "file": "plugins/cordova-plugin-device/www/device.js",
+      "pluginId": "cordova-plugin-device",
+      "clobbers": [
+        "device"
+      ]
+    }
+  ];
+  module.exports.metadata = {
+    "cordova-sqlite-storage": "6.1.0",
+    "cordova-plugin-device": "3.0.0"
+  };
+});
