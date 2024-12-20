b983adb9a991050d7c7d50eb3ab2479391b43237
Satish9047 <satishprajapati930@gmail.com>
2024-12-20 12:36:52 +0545

    176: add exec from cordova to communicated between javascript and java

--- task5-custom-http-plugin/HttpPlugin/www/HttpPlugin.js
+++ task5-custom-http-plugin/HttpPlugin/www/HttpPlugin.js
@@ -1,5 +1,5 @@
 var exec = require("cordova/exec");
 
 exports.getHttpRequest = function (url, success, error) {
-  exec(success, error, "HttpPlugin", "getHttpRequest", url);
+  exec(success, error, "HttpPlugin", "getHttpRequest", [url]);
 };
