a3c7fc62222c671b9f955b02fdfac35878f68405
Satish9047 <satishprajapati930@gmail.com>
2024-12-23 12:06:16 +0545

    179: Create Task Manager app using angular 19 +  add register, Login, dashboard + add basic route + html to the register and login

---
+++ task6-angular19/src/app/app.routes.ts
@@ -0,0 +1,31 @@
+import { Routes } from '@angular/router';
+import { LoginComponent } from './features/login/login.component';
+import { RegisterComponent } from './features/register/register.component';
+import { DashboardComponent } from './features/dashboard/dashboard.component';
+
+export const routes: Routes = [
+  {
+    path: 'login',
+    component: LoginComponent,
+    title: 'Login',
+  },
+  {
+    path: 'register',
+    component: RegisterComponent,
+    title: 'Register',
+  },
+  {
+    path: 'dashboard',
+    component: DashboardComponent,
+    title: 'Task Dashboard',
+  },
+  {
+    path: '',
+    redirectTo: '/login',
+    pathMatch: 'full',
+  },
+  {
+    path: '**',
+    redirectTo: '/login',
+  },
+];
