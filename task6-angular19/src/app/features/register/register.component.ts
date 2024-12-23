0bca566743048f4dcd8e417459733f36b366e7a0
Satish9047 <satishprajapati930@gmail.com>
2024-12-23 13:44:05 +0545

    188: Add Reactive Form Module to register form

--- task6-angular19/src/app/features/register/register.component.ts
+++ task6-angular19/src/app/features/register/register.component.ts
@@ -1,11 +1,44 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import {
+  FormGroup,
+  FormControl,
+  Validators,
+  ReactiveFormsModule,
+} from '@angular/forms';
 
 @Component({
   selector: 'app-register',
-  imports: [],
+  imports: [ReactiveFormsModule, CommonModule],
   templateUrl: './register.component.html',
-  styleUrl: './register.component.css'
+  styleUrl: './register.component.css',
 })
 export class RegisterComponent {
+  isLoading = false;
+  error: string | null = null;
 
+  constructor() {}
+
+  registerForm: FormGroup = new FormGroup({
+    firstName: new FormControl('', [
+      Validators.required,
+      Validators.minLength(2),
+    ]),
+    lastName: new FormControl('', [
+      Validators.required,
+      Validators.minLength(2),
+    ]),
+    email: new FormControl('', [Validators.required, Validators.email]),
+    password: new FormControl('', [
+      Validators.required,
+      Validators.minLength(8),
+    ]),
+  });
+
+  //unSubmit method
+  onSubmit(): void {
+    this.isLoading = true;
+    console.log(this.registerForm.value);
+    this.isLoading = false;
+  }
 }
