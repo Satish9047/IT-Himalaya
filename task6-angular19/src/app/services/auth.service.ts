78eea40c1d1e022c94cd40ed576f5370970fc651
Satish9047 <satishprajapati930@gmail.com>
2024-12-23 16:52:56 +0545

    199: Create the register User method and loginUser method

--- task6-angular19/src/app/services/auth.service.ts
+++ task6-angular19/src/app/services/auth.service.ts
@@ -1,9 +1,31 @@
+import { LocalforageService } from './localforage.service';
 import { Injectable } from '@angular/core';
+import { Response, User } from '../interface/interface';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class AuthService {
+  user: User | null = null;
+  constructor(private localforageService: LocalforageService) {}
 
-  constructor() { }
+  async registerUser(user: User): Promise<Response> {
+    const storeInstance = this.localforageService.getStoreInstance(user);
+    try {
+      await storeInstance.setItem('userData', user);
+      return { success: true, message: 'User saved to localForage.' };
+    } catch (error: any) {
+      return { success: false, message: error.message };
+    }
+  }
+
+  async loginUser(): Promise<Response> {
+    const storeInstance = this.localforageService.getLoggedUserStoreInstance();
+    try {
+      await storeInstance.setItem('loggedUser', this.user);
+      return { success: true, message: 'User logged in.' };
+    } catch (error: any) {
+      return { success: false, message: error.message };
+    }
+  }
 }
