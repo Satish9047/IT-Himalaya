31a49d044ca01a3ba10dd33051baecfffa341604
Satish9047 <satishprajapati930@gmail.com>
2024-12-23 17:42:16 +0545

    204: Update registerUser with data in response

--- task6-angular19/src/app/services/auth.service.ts
+++ task6-angular19/src/app/services/auth.service.ts
@@ -1,6 +1,6 @@
 import { LocalforageService } from './localforage.service';
 import { Injectable } from '@angular/core';
-import { Response, User } from '../interface/interface';
+import { LoginData, Response, User } from '../interface/interface';
 
 @Injectable({
   providedIn: 'root',
@@ -9,23 +9,42 @@ export class AuthService {
   user: User | null = null;
   constructor(private localforageService: LocalforageService) {}
 
-  async registerUser(user: User): Promise<Response> {
+  async registerUser(user: User): Promise<Response<User>> {
     const storeInstance = this.localforageService.getStoreInstance(user);
     try {
       await storeInstance.setItem('userData', user);
-      return { success: true, message: 'User saved to localForage.' };
+      return {
+        success: true,
+        message: 'User saved to localForage.',
+        data: null,
+      };
     } catch (error: any) {
-      return { success: false, message: error.message };
+      return { success: false, message: error.message, data: null };
     }
   }
 
-  async loginUser(): Promise<Response> {
-    const storeInstance = this.localforageService.getLoggedUserStoreInstance();
+  async loginUser(userData: LoginData): Promise<Response<User>> {
+    const storeInstance = this.localforageService.getStoreInstance(userData);
     try {
-      await storeInstance.setItem('loggedUser', this.user);
-      return { success: true, message: 'User logged in.' };
+      const existUser = await storeInstance.getItem<User>('userData');
+      if (!existUser) {
+        return { success: false, message: 'User not found.', data: existUser };
+      }
+      if (existUser.password !== userData.password) {
+        return { success: false, message: 'Invalid password.', data: null };
+      }
+
+      const loggerStoreInstance =
+        this.localforageService.getLoggedUserStoreInstance();
+      try {
+        await loggerStoreInstance.setItem('loggedUser', this.user);
+        return { success: true, message: 'User logged in.', data: existUser };
+      } catch (error: any) {
+        return { success: false, message: error.message, data: null };
+      }
     } catch (error: any) {
-      return { success: false, message: error.message };
+      console.log(error);
+      return { success: false, message: error.message, data: null };
     }
   }
 }
