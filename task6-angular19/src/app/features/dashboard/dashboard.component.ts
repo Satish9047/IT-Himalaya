400aa331d04f952e3695a774f3827465c756a4fc
Satish9047 <satishprajapati930@gmail.com>
2024-12-23 12:43:40 +0545

    181: Add child component addTask, todoTask and completed Task in the dashboard

--- task6-angular19/src/app/features/dashboard/dashboard.component.ts
+++ task6-angular19/src/app/features/dashboard/dashboard.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
+import { AddTaskComponent } from './add-task/add-task.component';
+import { TodoTasksComponent } from './todo-tasks/todo-tasks.component';
+import { CompletedTasksComponent } from './completed-tasks/completed-tasks.component';
 
 @Component({
   selector: 'app-dashboard',
-  imports: [],
+  imports: [AddTaskComponent, TodoTasksComponent, CompletedTasksComponent],
   templateUrl: './dashboard.component.html',
-  styleUrl: './dashboard.component.css'
+  styleUrl: './dashboard.component.css',
 })
-export class DashboardComponent {
-
-}
+export class DashboardComponent {}
