cb9807b2736b23d49e1aba1f6dee5e3723534f43
Satish9047 <satishprajapati930@gmail.com>
2024-12-24 15:23:12 +0545

    220: Add fetch userData from localForage in set it in the constructor

--- task6-angular19/src/app/services/user.service.ts
+++ task6-angular19/src/app/services/user.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { WritableSignal, signal } from '@angular/core';
 
 import { User } from '../interface/interface';
+import { LocalforageService } from './localforage.service';
 
 @Injectable({
   providedIn: 'root',
@@ -10,7 +11,13 @@ export class UserService {
   private _user: WritableSignal<User | null> = signal<User | null>(null);
   readonly user = this._user.asReadonly();
 
-  constructor() {}
+  constructor(private localforageService: LocalforageService) {
+    this.localforageService.getLoggedUser().then((user) => {
+      if (user) {
+        this.setUser(user);
+      }
+    });
+  }
 
   setUser(user: User) {
     this._user.set(user);
