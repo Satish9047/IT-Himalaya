fdb9fd47cb6b90b15cf3c3dd9eff6abb8e56c14f
Satish9047 <satishprajapati930@gmail.com>
2024-12-24 13:28:00 +0545

    212: Update TaskService with signal + addTask + markTaskAsCompleted+ deleteTask method for the service

--- task6-angular19/src/app/services/task.service.ts
+++ task6-angular19/src/app/services/task.service.ts
@@ -1,9 +1,31 @@
-import { Injectable } from '@angular/core';
+import { Injectable, signal, WritableSignal } from '@angular/core';
+import { Task } from '../core/task';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class TaskService {
+  private _tasks: WritableSignal<Task[]> = signal([]);
+  readonly tasks = this._tasks.asReadonly();
 
-  constructor() { }
+  constructor() {}
+
+  addTask(task: Task): void {
+    this._tasks.update((tasks) => [...tasks, task]);
+  }
+
+  markTaskAsCompleted(taskId: string): void {
+    this._tasks.update((tasks) => {
+      return tasks.map((task) => {
+        if (task.id === taskId) {
+          task.MaskTaskToComplete();
+        }
+        return task;
+      });
+    });
+  }
+
+  deleteTask(taskId: string): void {
+    this._tasks.update((tasks) => tasks.filter((task) => task.id !== taskId));
+  }
 }
