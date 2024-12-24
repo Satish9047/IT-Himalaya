b221745e5e61bb9ab482d7d246f1562311cf743a
Satish9047 <satishprajapati930@gmail.com>
2024-12-24 11:38:15 +0545

    207: Add user as a signal

--- task6-angular19/src/app/services/user.service.ts
+++ task6-angular19/src/app/services/user.service.ts
@@ -1,23 +1,22 @@
 import { Injectable } from '@angular/core';
+import { WritableSignal, signal } from '@angular/core';
+
 import { User } from '../interface/interface';
 
 @Injectable({
   providedIn: 'root',
 })
 export class UserService {
-  user: User | null = null;
+  private _user: WritableSignal<User | null> = signal<User | null>(null);
+  readonly user = this._user.asReadonly();
 
   constructor() {}
 
   setUser(user: User) {
-    this.user = user;
-  }
-
-  getUser() {
-    return this.user;
+    this._user.set(user);
   }
 
   clearUser() {
-    this.user = null;
+    this._user.set(null);
   }
 }
