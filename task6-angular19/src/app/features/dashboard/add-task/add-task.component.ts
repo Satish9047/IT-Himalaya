57c55738293e0fd8e956043c973d4dd47b36c79a
Satish9047 <satishprajapati930@gmail.com>
2024-12-24 13:25:26 +0545

    211: Add FormGroup and formControl + addTask method

--- task6-angular19/src/app/features/dashboard/add-task/add-task.component.ts
+++ task6-angular19/src/app/features/dashboard/add-task/add-task.component.ts
@@ -1,11 +1,37 @@
 import { Component } from '@angular/core';
+import {
+  FormGroup,
+  FormControl,
+  Validators,
+  ReactiveFormsModule,
+} from '@angular/forms';
+
+import { TaskService } from '../../../services/task.service';
+import { Task } from '../../../core/task';
 
 @Component({
   selector: 'app-add-task',
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './add-task.component.html',
-  styleUrl: './add-task.component.css'
+  styleUrl: './add-task.component.css',
 })
 export class AddTaskComponent {
+  constructor(private taskService: TaskService) {}
 
+  addTaskForm: FormGroup = new FormGroup({
+    taskDescription: new FormControl('', [
+      Validators.required,
+      Validators.minLength(2),
+    ]),
+  });
+
+  addTask() {
+    console.log(this.addTaskForm.value);
+    //add new task
+    const taskId = Date.now().toString();
+    const newTask = new Task(taskId, this.addTaskForm.value.taskDescription);
+    this.taskService.addTask(newTask);
+
+    this.addTaskForm.reset();
+  }
 }
