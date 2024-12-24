33fedb60b9739d3e7013ec22e65b95c4a8993923
Satish9047 <satishprajapati930@gmail.com>
2024-12-24 13:32:02 +0545

    214: Add todoTasks as a computed signal + Add deleteTask method + completedTask method + Update the template with proper data and use of signal

--- task6-angular19/src/app/features/dashboard/todo-tasks/todo-tasks.component.ts
+++ task6-angular19/src/app/features/dashboard/todo-tasks/todo-tasks.component.ts
@@ -1,11 +1,31 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, computed, Signal } from '@angular/core';
+
+import { Task } from '../../../core/task';
+import { TaskService } from '../../../services/task.service';
 
 @Component({
   selector: 'app-todo-tasks',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './todo-tasks.component.html',
-  styleUrl: './todo-tasks.component.css'
+  styleUrl: './todo-tasks.component.css',
 })
 export class TodoTasksComponent {
+  tasks: Signal<Task[]>;
+  todoTasks: Signal<Task[]>;
+  constructor(private taskService: TaskService) {
+    this.tasks = this.taskService.tasks;
 
+    this.todoTasks = computed(() =>
+      this.tasks().filter((task) => !task.completed),
+    );
+  }
+
+  deleteTask(taskId: string): void {
+    this.taskService.deleteTask(taskId);
+  }
+
+  completedTask(taskId: string): void {
+    this.taskService.markTaskAsCompleted(taskId);
+  }
 }
