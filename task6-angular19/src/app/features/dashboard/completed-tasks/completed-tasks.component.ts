7ca81729e2b49d60dc92055c913b4429b0f2696d
Satish9047 <satishprajapati930@gmail.com>
2024-12-24 13:30:29 +0545

    213: Add the completedTask as a computed signal + Add deleteCompletedTask method + Update the template with proper data and use of signal

--- task6-angular19/src/app/features/dashboard/completed-tasks/completed-tasks.component.ts
+++ task6-angular19/src/app/features/dashboard/completed-tasks/completed-tasks.component.ts
@@ -1,11 +1,27 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, computed, Signal } from '@angular/core';
+
+import { Task } from '../../../core/task';
+import { TaskService } from '../../../services/task.service';
 
 @Component({
   selector: 'app-completed-tasks',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './completed-tasks.component.html',
-  styleUrl: './completed-tasks.component.css'
+  styleUrl: './completed-tasks.component.css',
 })
 export class CompletedTasksComponent {
+  tasks: Signal<Task[]>;
+  completedTasks: Signal<Task[]>;
+  constructor(private taskService: TaskService) {
+    this.tasks = this.taskService.tasks;
 
+    this.completedTasks = computed(() => {
+      return this.tasks().filter((task) => task.completed);
+    });
+  }
+
+  deleteCompletedTask(taskId: string): void {
+    this.taskService.deleteTask(taskId);
+  }
 }
