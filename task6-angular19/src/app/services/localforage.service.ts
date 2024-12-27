e7c205d297cdb3621dc99111d8bcbd14c0eb098b
Satish9047 <satishprajapati930@gmail.com>
2024-12-27 12:15:46 +0545

    243: Update the localStorageService with proper loggedUserStore and the initialize method

--- task6-angular19/src/app/services/localforage.service.ts
+++ task6-angular19/src/app/services/localforage.service.ts
@@ -7,11 +7,32 @@ import { LoginData, User } from '../interface/interface';
   providedIn: 'root',
 })
 export class LocalforageService {
+  private loggedUserStore = localForage.createInstance({
+    name: `loggedUser`,
+    storeName: 'user',
+    description: `Data for logged user`,
+  });
+
   constructor() {}
+
+  getLoggedUserStoreInstance() {
+    return this.loggedUserStore;
+  }
+
+  async initialize(): Promise<User | null> {
+    try {
+      const user = await this.getLoggedUser();
+      console.log('User from LocalforageService:', user);
+      return user;
+    } catch (error) {
+      console.error('Error initializing LocalForage:', error);
+      return null;
+    }
+  }
 
   async getLoggedUser() {
     const storeInstance = this.getLoggedUserStoreInstance();
-    const user = await storeInstance.getItem<User>('userData');
+    const user = await storeInstance.getItem<User>('loggedUser');
     if (user) {
       return user;
     } else {
@@ -25,15 +46,6 @@ export class LocalforageService {
       name: `Users`,
       storeName,
       description: `Data for ${user.email}`,
-    });
-    return storeInstance;
-  }
-
-  getLoggedUserStoreInstance() {
-    const storeInstance = localForage.createInstance({
-      name: `loggedUser`,
-      storeName: 'user',
-      description: `Data for logged user`,
     });
     return storeInstance;
   }
