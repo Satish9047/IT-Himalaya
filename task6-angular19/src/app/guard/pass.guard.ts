a2802af88955eb36ec8f3a1cfb8a497bfd1edbfa
Satish9047 <satishprajapati930@gmail.com>
2024-12-27 12:14:42 +0545

    242: Update the guard by using signal

--- task6-angular19/src/app/guard/pass.guard.ts
+++ task6-angular19/src/app/guard/pass.guard.ts
@@ -1,19 +1,26 @@
-import { Injectable } from '@angular/core';
+import { Injectable, Signal, computed } from '@angular/core';
 import { CanActivate, Router } from '@angular/router';
 
 import { UserService } from '../services/user.service';
+import { User } from '../interface/interface';
 
 @Injectable({
   providedIn: 'root',
 })
 export class PassGuard implements CanActivate {
+  private user: Signal<User | null>;
+
   constructor(
     private userService: UserService,
     private router: Router,
-  ) {}
+  ) {
+    this.user = this.userService.user;
+  }
 
-  canActivate(): boolean {
-    if (this.userService.user()) {
+  async canActivate(): Promise<boolean> {
+    this.user = this.userService.user;
+    if (this.user()) {
+      console.log('canActivate guard', this.userService.user());
       this.router.navigate(['/dashboard']);
       return false;
     }
