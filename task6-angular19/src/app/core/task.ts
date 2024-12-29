10c993bab6855d4f3be26de9ce4a2e42e00b8cb0
Satish9047 <satishprajapati930@gmail.com>
2024-12-29 21:14:46 +0545

    250: Update Task Class with user Id

--- task6-angular19/src/app/core/task.ts
+++ task6-angular19/src/app/core/task.ts
@@ -1,28 +1,30 @@
 import { getFormattedDate, getDueDate } from '../utils/date';
 
 export class Task {
-  id: string;
+  id?: string | number;
   description: string;
   createdAt: string;
   dueDate: string;
   completed: boolean;
   completedAt: string | null;
-  userId?: string;
+  userId: string | number;
 
   constructor(
-    id: string,
+    userId: string | number,
     description: string,
     createdAt: string,
     dueDate: string,
     completed = false,
     completedAt: string | null = null,
+    id: string | number | undefined = undefined,
   ) {
-    this.id = id;
+    this.userId = userId;
     this.description = description;
     this.completed = completed;
     this.dueDate = dueDate;
     this.createdAt = createdAt;
     this.completedAt = completedAt;
+    this.id = id;
   }
 
   MarkTaskToCompleted() {
