8e163b77a7bf5a5191e8ee62b043b012ba6fbf2a
Satish9047 <satishprajapati930@gmail.com>
2024-12-29 21:30:35 +0545

    261: Update task method by using DexieDB instead of localForage

--- task6-angular19/src/app/services/task.service.ts
+++ task6-angular19/src/app/services/task.service.ts
@@ -6,9 +6,9 @@ import {
   WritableSignal,
 } from '@angular/core';
 import { Task } from '../core/task';
-import { LocalforageService } from './localforage.service';
 import { UserService } from './user.service';
 import { User } from '../interface/interface';
+import { DexieTaskService } from './dexie-task.service';
 
 @Injectable({
   providedIn: 'root',
@@ -20,8 +20,8 @@ export class TaskService {
   currentUser!: User | null;
 
   constructor(
-    private localforageService: LocalforageService,
     private userService: UserService,
+    private dexieTaskService: DexieTaskService,
   ) {
     this.user = this.userService.user;
     effect(() => {
@@ -30,33 +30,28 @@ export class TaskService {
   }
 
   async loadTasks(): Promise<Task[]> {
-    if (this.currentUser === null) {
+    console.log('current user', this.currentUser);
+    if (!this.currentUser || this.currentUser.id === null) {
       return [];
     }
     try {
-      const storeInstance = this.localforageService.getStoreInstance(
-        this.currentUser,
-      );
-      const tasks = await storeInstance.getItem<Task[]>('tasks');
-      if (tasks) {
-        const taskLoad: Task[] = [];
-        tasks.forEach((task) => {
-          const newTask = new Task(
-            task.id,
-            task.description,
-            task.createdAt,
-            task.dueDate,
-            task.completed,
-            task.completedAt,
-          );
-          taskLoad.push(newTask);
-        });
-        this._tasks.set(taskLoad);
-        return tasks;
-      } else {
-        return [];
-      }
+      const res = await this.dexieTaskService.getUserTasks(this.currentUser.id);
+      console.log('dexie task response data', res);
+      const tasks = res.map((task) => {
+        return new Task(
+          task.userId,
+          task.description,
+          task.createdAt,
+          task.dueDate,
+          task.completed,
+          task.completedAt,
+          task.id,
+        );
+      });
+      this._tasks.set(tasks);
+      return res;
     } catch (error) {
+      console.log('error while loading tasks');
       return [];
     }
   }
@@ -66,22 +61,9 @@ export class TaskService {
       return false;
     }
     this._tasks.update((tasks) => [...tasks, task]);
-
     try {
-      if (this.currentUser) {
-        const storeInstance = this.localforageService.getStoreInstance(
-          this.currentUser,
-        );
-        const res = await storeInstance.setItem('tasks', this._tasks());
-        if (res) {
-          console.log('addTask in localforage', res);
-          return true;
-        } else {
-          return false;
-        }
-      } else {
-        return false;
-      }
+      await this.dexieTaskService.addTask(task);
+      return true;
     } catch (error) {
       return false;
     }
@@ -90,50 +72,75 @@ export class TaskService {
   async markTaskAsCompleted(taskId: string) {
     this._tasks.update((tasks) =>
       tasks.map((task) => {
-        if (task.id === taskId) {
+        if (task.id === taskId && this.currentUser) {
           task.MarkTaskToCompleted();
-          if (this.currentUser) {
-            const storeInstance = this.localforageService.getStoreInstance(
-              this.currentUser,
-            );
-            storeInstance
-              .setItem('tasks', tasks) // Use original tasks array
-              .then((res) => {
-                if (res) {
-                  console.log('update Task in localforage', res);
-                }
-              })
-              .catch((error) => {
-                console.error('Error updating tasks in localforage', error);
-              });
-          }
+          this.dexieTaskService.updateTaskToCompleted(
+            task,
+            this.currentUser.id,
+          );
         }
         return task;
       }),
     );
+    // this._tasks.update((tasks) =>
+    //   tasks.map((task) => {
+    //     if (task.id === taskId) {
+    //       task.MarkTaskToCompleted();
+    //       if (this.currentUser) {
+    //         const storeInstance = this.localforageService.getStoreInstance(
+    //           this.currentUser,
+    //         );
+    //         storeInstance
+    //           .setItem('tasks', tasks) // Use original tasks array
+    //           .then((res) => {
+    //             if (res) {
+    //               console.log('update Task in localforage', res);
+    //             }
+    //           })
+    //           .catch((error) => {
+    //             console.error('Error updating tasks in localforage', error);
+    //           });
+    //       }
+    //     }
+    //     return task;
+    //   }),
+    // );
   }
 
   async deleteTask(taskId: string) {
-    this._tasks.update((tasks) => tasks.filter((task) => task.id !== taskId));
-    try {
-      if (this.currentUser) {
-        const storeInstance = this.localforageService.getStoreInstance(
-          this.currentUser,
-        );
-        const res = await storeInstance.setItem('tasks', this._tasks());
-        if (res) {
-          console.log('delete Task in localforage', res);
-          return true;
-        } else {
-          return false;
-        }
-      } else {
-        return false;
-      }
-    } catch (error) {
-      console.error('Error deleting tasks in localforage', error);
-      return false;
+    if (!this.currentUser) {
+      return;
     }
+    this._tasks.update((tasks) => tasks.filter((task) => task.id !== taskId));
+
+    try {
+      const res = await this.dexieTaskService.deleteTask(
+        taskId,
+        this.currentUser.id,
+      );
+      console.log('delete Task in Dexie', res);
+    } catch (error) {
+      console.log('Error deleting tasks in localforage', error);
+    }
+    // try {
+    //   if (this.currentUser) {
+    //     const storeInstance = this.localforageService.getStoreInstance(
+    //       this.currentUser,
+    //     );
+    //     const res = await storeInstance.setItem('tasks', this._tasks());
+    //     if (res) {
+    //       console.log('delete Task in localforage', res);
+    //       return true;
+    //     } else {
+    //       return false;
+    //     }
+    //   } else {
+    //     return false;
+    //   }
+    // } catch (error) {
+    //   console.error('Error deleting tasks in localforage', error);
+    //   return false;
+    // }
   }
 
   clearTasks() {
