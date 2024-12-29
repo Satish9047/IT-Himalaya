0b4d987b38e2b5d1015266263242868b2bfea813
Satish9047 <satishprajapati930@gmail.com>
2024-12-29 21:24:20 +0545

    156: Use DexieDB instead of localForage + Remove unwanted logs

--- task6-angular19/src/app/services/auth.service.ts
+++ task6-angular19/src/app/services/auth.service.ts
@@ -1,79 +1,93 @@
 import { Injectable } from '@angular/core';
 
 import { UserService } from './user.service';
-import { LocalforageService } from './localforage.service';
 import { LoginData, Response, User } from '../interface/interface';
+import { DexieUserService } from './dexie-user.service';
 
 @Injectable({
   providedIn: 'root',
 })
 export class AuthService {
   constructor(
-    private localforageService: LocalforageService,
     private userService: UserService,
+    private dexieUserService: DexieUserService,
   ) {
     this.autoLogin();
   }
 
   async registerUser(user: User): Promise<Response<User>> {
-    const storeInstance = this.localforageService.getStoreInstance(user);
     try {
-      const res = await storeInstance.setItem('userData', user);
+      const res = await this.dexieUserService.AddUser(user);
       if (res) {
         return {
           success: true,
-          message: 'User saved to localForage.',
+          message: 'User saved to indexedDB.',
           data: null,
         };
       } else {
         return {
           success: false,
-          message: 'User not saved to localForage.',
+          message: 'Fail to save localForage.',
           data: null,
         };
       }
-    } catch (error: any) {
-      return { success: false, message: error.message, data: null };
+    } catch (error) {
+      console.log('error while registering');
+      return {
+        success: false,
+        message: 'Fail to save localForage.',
+        data: null,
+      };
     }
   }
 
   async loginUser(userData: LoginData): Promise<Response<User>> {
-    const storeInstance = this.localforageService.getStoreInstance(userData);
     try {
-      const existUser = await storeInstance.getItem<User>('userData');
-      if (!existUser) {
-        return { success: false, message: 'User not found.', data: null };
+      const res = await this.dexieUserService.getUserByEmail(userData);
+      if (res) {
+        if (userData.password === res.password) {
+          this.userService.setUser(res);
+          localStorage.setItem('LoggedUser', JSON.stringify(res));
+          return {
+            success: true,
+            message: 'Login Successful',
+            data: res,
+          };
+        } else {
+          return {
+            success: false,
+            message: 'Invalid Credentials',
+            data: null,
+          };
+        }
+      } else {
+        return {
+          success: false,
+          message: 'Login Unsuccessful',
+          data: null,
+        };
       }
-      if (existUser.password !== userData.password) {
-        return { success: false, message: 'Invalid password.', data: null };
-      }
-      //save the user data in the logged user store
-      const loggerStoreInstance =
-        this.localforageService.getLoggedUserStoreInstance();
-      try {
-        await loggerStoreInstance.setItem('loggedUser', existUser);
-        this.userService.setUser(existUser);
-        return { success: true, message: 'User logged in.', data: existUser };
-      } catch (error: any) {
-        return { success: false, message: error.message, data: null };
-      }
-    } catch (error: any) {
-      console.log(error);
-      return { success: false, message: error.message, data: null };
+    } catch (error) {
+      console.log('error while logging in');
+      return {
+        success: false,
+        message: 'Login Unsuccessful',
+        data: null,
+      };
     }
   }
 
-  async autoLogin(): Promise<User | null> {
+  autoLogin(): User | null {
     try {
-      const storeInstance =
-        this.localforageService.getLoggedUserStoreInstance();
-      const user = await storeInstance.getItem<User>('loggedUser');
-      if (user) {
+      const loggedUser = localStorage.getItem('LoggedUser');
+      if (loggedUser) {
+        const user: User = JSON.parse(loggedUser);
         this.userService.setUser(user);
+        return user;
       }
-      return user;
+      return null;
     } catch (error) {
-      console.error('Error during auto-login:', error);
+      console.log('error while auto login', error);
       return null;
     }
   }
