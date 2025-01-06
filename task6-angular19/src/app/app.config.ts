1443572b8afa274bef2faf1a559c430858a9c1a6
Satish9047 <satishprajapati930@gmail.com>
2025-01-06 12:03:28 +0545

    302: Update pwa

--- task6-angular19/src/app/app.config.ts
+++ task6-angular19/src/app/app.config.ts
@@ -1,6 +1,5 @@
 import {
   ApplicationConfig,
-  provideZoneChangeDetection,
   isDevMode,
   provideExperimentalZonelessChangeDetection,
 } from '@angular/core';
@@ -16,9 +15,6 @@ export const appConfig: ApplicationConfig = {
     provideServiceWorker('ngsw-worker.js', {
       enabled: !isDevMode(),
       registrationStrategy: 'registerWhenStable:30000',
-    }), provideServiceWorker('ngsw-worker.js', {
-            enabled: !isDevMode(),
-            registrationStrategy: 'registerWhenStable:30000'
-          }),
+    }),
   ],
 };
