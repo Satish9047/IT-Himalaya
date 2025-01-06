c4aed9449802a9cedd324611e30eb00273adca78
Satish9047 <satishprajapati930@gmail.com>
2025-01-06 11:36:19 +0545

    301: update the capacitor webDir path + add android capacitor platform

---
+++ task6-angular19/android/app/src/main/java/com/taskManager/app/MainActivity.java
@@ -0,0 +1,5 @@
+package com.taskManager.app;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
--- task6-angular19/capacitor.config.ts
+++ task6-angular19/capacitor.config.ts
@@ -3,7 +3,7 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'com.taskManager.app',
   appName: 'task6-angular19',
-  webDir: 'www/task6-angular19/browser',
+  webDir: 'dist/task6-angular19/browser',
 };
 
 export default config;
